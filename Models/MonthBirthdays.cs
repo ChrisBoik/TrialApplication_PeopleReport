73c04f3d100abac64025bfbaf3f523cd9dbb4453
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:42:33 +0200

    Add Person and MonthBirthdays models

---
+++ Models/MonthBirthdays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrialApplication_PeopleReport.Models
+{
+    public class MonthBirthdays
+    {
+        public int Month { get; set; }
+        public int Num_Month_Birthdays { get; set; }
+        public MonthBirthdays(int month, int numMonthBirthdays)
+        {
+            Month = month;
+            Num_Month_Birthdays = numMonthBirthdays;
+        }
+    }
+}
--- Models/Person.cs
+++ Models/Person.cs
@@ -1,13 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrialApplication_PeopleReport.Models
 {
     public class Person
     {
+        public int Id { get; set; }
+
         public string First_Name { get; set; }
 
-        public int Last_name { get; set; }
+        public string Last_Name { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime Date_of_Birth { get; set; }
 
         public int Age
@@ -15,7 +19,7 @@ namespace TrialApplication_PeopleReport.Models
             get
             {
                 int age = (int)(DateTime.Today.Year - Date_of_Birth.Year);
-                if (Date_of_Birth > DateTime.Today.AddYears(-age)) age--;
+                if (Date_of_Birth.Date > DateTime.Today.AddYears(-age)) age--;
                 return age;
             }
         }
