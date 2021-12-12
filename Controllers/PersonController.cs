2894a18e0dae1f4f779378af408f3d1af0e3f5a9
Christopher Boik <45264553+MysticZA@users.noreply.github.com>
2021-12-12 16:44:14 +0200

    Add Person controller & more
    - Add fetch people endpoint (GET)
    - Add edit person endpoint (PUT)
    - Add delete person endpoint (DELETE)
    - Add fetch birthdays in months endpoint (GET)

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using TrialApplication_PeopleReport.AppDbContext;
+using TrialApplication_PeopleReport.Models;
 
 namespace TrialApplication_PeopleReport.Controllers
 {
@@ -11,24 +15,88 @@ namespace TrialApplication_PeopleReport.Controllers
     [Route("[controller]")]
     public class PersonController : ControllerBase
     {
+        private readonly PersonContext _context;
         private readonly ILogger<PersonController> _logger;
 
-        public PersonController(ILogger<PersonController> logger)
+        public PersonController(ILogger<PersonController> logger, PersonContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Person> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new Person
+            return _context.People.ToArray();
+        }
+
+        [HttpGet]
+        [Route("birthdays")]
+        public List<MonthBirthdays> GetBirthdays()
+        {
+            return _context.People.GroupBy(p => p.Date_of_Birth.Month).Select(group =>
+            new MonthBirthdays(group.Key, group.Count())).ToList();
+        }
+
+        [HttpPost]
+        //[Route("test")]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            _context.Add(person);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(PostPerson), new { id = person.Id }, person);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePerson(int id)
+        {
+            var person = await _context.People.FindAsync(id);
+            if (person == null)
             {
-                First_Name = DateTime.Now.AddDays(index),
-                Last_name = rng.Next(-20, 55),
-                Date_of_Birth = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound();
+            }
+
+            _context.People.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditPerson(int id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.People.Any(e => e.Id == id);
         }
     }
+
 }
